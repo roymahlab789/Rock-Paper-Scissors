bb09510ea9b08e03cd2e75d32855017ce8feeb07
roymahlab789 <roymahlab67@gmail.com>
2025-08-24 22:19:49 +0300

    finished the game

--- javascript.js
+++ javascript.js
@@ -20,16 +20,29 @@ let choice = prompt("Please type rock, paper or scissors:");
 return choice;
 }
 console.log(getHumanChoice())
-let humanScore = 0
-let computerScore = 0
 
-function playRound(humanChoice, computerChoice){
-humanChoice = humanChoice.toLowerCase();
-console.log("You lose! Paper beats Rock");
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = humanChoice.toLowerCase();
+        console.log("You lose! Paper beats Rock"); // replace with full logic later
+        // increment humanScore or computerScore depending on who wins
+    }
+
+    // loop for 5 rounds
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+
+    // final winner
+    if (humanScore > computerScore) console.log("You won the game!");
+    else if (computerScore > humanScore) console.log("Computer won the game!");
+    else console.log("The game is a tie!");
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice
-
-playRound(humanSelection, computerSelection)
-
+// start the game
+playGame();
